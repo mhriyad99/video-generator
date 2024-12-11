d4cfcea66cea0eb44098131ec232b761a4995909
mhriyad99 <mhriyad98@gmail.com>
2024-12-11 16:07:19 +0600

    dec_wed_11: second commit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
 from src.video_editor.video_utils import download_video, get_video_transcript, cut_clips, join_clips, add_audio_to_video_ffmpeg
 
-# os.makedirs("./downloaded_videos", exist_ok=True)
-# os.makedirs("./clips", exist_ok=True)
-# video_url = "https://www.youtube.com/watch?v=W7ppd_RY-UE"
-#
-# transcript= get_video_transcript(video_url)
-# title = download_video(video_url, download_dir="downloaded_videos")
-#
-# video_path = os.path.join("downloaded_videos", title + ".mp4")
-# output_dir = cut_clips(video_path, title, transcript, clips_num=3)
-# join_clips(output_dir, "output2.mp4")
+os.makedirs("./downloaded_videos", exist_ok=True)
+os.makedirs("./clips", exist_ok=True)
+video_url = "https://www.youtube.com/watch?v=W7ppd_RY-UE"
+
+transcript= get_video_transcript(video_url)
+title = download_video(video_url, download_dir="downloaded_videos")
+
+video_path = os.path.join("downloaded_videos", title + ".mp4")
+output_dir = cut_clips(video_path, title, transcript, clips_num=3)
+join_clips(output_dir, "output2.mp4")
 add_audio_to_video_ffmpeg("output.mp4", "Rome.mp3", "final.mp4")
 
 
